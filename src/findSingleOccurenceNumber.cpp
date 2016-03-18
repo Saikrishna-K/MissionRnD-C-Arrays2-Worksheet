95daf95f7c972fcc6f656d853ba33027740b7586
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-03-18 13:40:37 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 /*
 OVERVIEW: Given an array where every element occurs three times, except one element which occurs only
 once. Find the element that occurs once.
@@ -15,6 +16,58 @@ There are better ways of solving the problem than a brute-force solution which i
 complexity .
 */
 
+// Sorting the given Array using quickSort -->  (O(nlogn))
+void quickSort(int *arr, int left, int right)
+{
+
+	int pivot = arr[right], i, leftIndex = left, rightIndex = right;
+
+	while (leftIndex<rightIndex){
+
+		while (arr[leftIndex] < pivot && leftIndex < rightIndex){ leftIndex++; }
+		if (leftIndex != rightIndex)  arr[rightIndex] = arr[leftIndex];
+
+		while (arr[rightIndex] >= pivot && leftIndex < rightIndex){ rightIndex--; }
+		if (leftIndex != rightIndex) arr[leftIndex] = arr[rightIndex];
+
+	}
+
+	arr[leftIndex] = pivot;
+
+	pivot = leftIndex;
+	if (left<pivot)
+		quickSort(arr, left, pivot - 1);
+	if (pivot<right)
+		quickSort(arr, pivot + 1, right);
+
+}
+
+
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+
+	if (A == NULL || len < 1) return -1;
+
+	int i,left = 0, right = len - 1, mid;
+	quickSort(A, left, right);
+ 
+	for (i = 0; i < len - 3; i++)
+	{
+
+		if (A[i] != A[i + 2]) // if the current and the next 2nd element is not same then return that element
+		{
+			return A[i];
+		}
+		else
+		{
+			i = i + 2;
+		}
+
+
+	}
+	// if the Single Occurence element is at the last then return last element
+	return A[len - 1];
+
+
+
+
 }
--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -22,6 +22,119 @@ struct transaction {
 	char description[20];
 };
 
+int compare2(char *date1, char *date2) {// compares the date and returns the value accordingly 
+
+	int i, year1 = 0, year2 = 0;
+
+
+	for (i = 6; i<10; i++)
+	{
+		year1 *= 10;
+		year2 *= 10;
+		year1 += date1[i] - '0';
+		year2 += date2[i] - '0';
+
+
+	}
+
+
+
+	int mon1 = 0, mon2 = 0;
+	for (i = 3; i < 5; i++)
+	{
+		mon1 *= 10;
+		mon2 *= 10;
+		mon1 += date1[i] - '0';
+		mon2 += date2[i] - '0';
+	}
+	if (mon1 >12 || mon2> 12) return -1;
+
+
+	int	day1 = 0, day2 = 0;
+	for (i = 0; i < 2; i++)
+	{
+		day1 *= 10;
+		day2 *= 10;
+		day1 += date1[i] - '0';
+		day2 += date2[i] - '0';
+	}
+
+
+
+	if (year1 < year2)
+	{
+		return 1;
+	}
+	else if (year1 > year2)
+		return 2;
+
+	if (mon1 < mon2)
+	{
+		return 1;
+	}
+	else if (mon2 < mon1)
+		return 2;
+
+
+	if (day1 < day2)
+	{
+		return 1;
+	}
+	else if (day2 < day1)
+		return 2;
+
+
+	return 0;
+}
+
+
+
+
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+
+	if (ALen < 1 || BLen < 1 || A == NULL || B == NULL) return NULL;
+
+	int TLen = ALen + BLen, i = 0, j = 0, k = 0, value;
+
+	struct transaction *result = (struct transaction*)calloc(TLen, sizeof(struct transaction));
+
+	while (i != ALen && j != BLen)// copies the values into result array in ascending order
+	{
+		value = compare2(A[i].date, B[j].date);
+		if (value == 0)
+		{
+			result[k++] = A[i++];
+			result[k++] = B[j++];
+
+		}
+		else if (value == 1)
+		{
+			result[k++] = A[i++];
+		}
+		else if (value == 2)
+		{
+			result[k++] = B[j++];
+		}
+
+	}
+
+	if (i < ALen)
+	{
+		while (i != ALen)
+		{
+			result[k++] = A[i++];
+		}
+
+	}
+	else if (j != BLen)
+	{
+
+		while (j != BLen)
+		{
+			result[k++] = B[j++];
+		}
+	}
+
+
+	return result;
 }
